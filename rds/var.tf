4ff13238be910d9d72b1c03cf71a5c60738742df
Irene <imangan@hotmail.com>
2023-11-08 21:35:15 -0600

    adding vpc, rds code

---
+++ rds/var.tf
@@ -0,0 +1,19 @@
+variable "private_subnet1" {
+  type = string
+  description = "id of subnet"
+}
+
+variable "private_subnet2" {
+  type = string
+  description = "id of subnet"
+}
+
+variable "tags" {
+    type = map(string)
+    description = "tags"
+}
+
+variable "vpc_cidr" {
+  type = string
+  description = "cidr range"
+}
